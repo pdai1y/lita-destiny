e65cd9ab61e005b0cfa813674b60a04636b8929b
pDaily <pat@pdaily.me>
2015-08-31 18:14:16 -0400

    Updated specs

--- spec/lita/handlers/destiny_spec.rb
+++ spec/lita/handlers/destiny_spec.rb
@@ -1,13 +1,22 @@
 require "spec_helper"
 
-describe Lita::Handlers::Destiny, lita_handler: true do
+describe Lita::Handlers::DestinyHandler, lita_handler: true do
   let(:api_key) { 'd319ebf361e9f4f28e65fa05ce1cf8d6' }
-  let(:config) { Lita::Handlers::Destiny.configuration_builder.build }
-  
+  let(:config) { Lita::Handlers::DestinyHandler.configuration_builder.build }
+
   before do
     config.api_key = api_key
   end
-  
+
   it { is_expected.to route_command("!nightfall").to(:nightfall)}
-  
+
+  it { is_expected.to route_command("!weekly").to(:weekly)}
+
+  it { is_expected.to route_command("!xur").to(:xur)}
+
+  it { is_expected.to route_command("!poe32").to(:poe_32)}
+
+  it { is_expected.to route_command("!poe34").to(:poe_34)}
+
+  it { is_expected.to route_command("!poe35").to(:poe_35)}
 end
